1a84dfea00ba429c1ab64ff6acf6338822a4db2f
nativealien <oskar@assarsson.nu>
2022-10-21 23:25:38 +0200

    First commit

---
+++ event.js
@@ -0,0 +1,19 @@
+// import { game } from "./game.js"
+
+export let mouse = {fire: false, x: 0, y: 0}
+
+export function mousePos(event){
+    mouse.x = event.x
+    mouse.y = event.y
+}
+export function mouseDown(event){
+    if (event.repeat) { return}
+    if (event){
+        mouse.fire = true
+    }
+}
+export function mouseUp(event){
+    if (event){
+        mouse.fire = false
+    }
+}
